bee0e5ce2c4a1c91409bbd75a963310b76245612
hugosenari <hugosenari@gmail.com>
2022-03-12 02:28:41 -0300

    chore: re index funchook

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''funchook wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."funchook-master".url = "path:./master";
+  inputs."funchook-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."funchook-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."funchook-0_1_1".url = "path:./0_1_1";
+  inputs."funchook-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."funchook-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."funchook-0_1_2".url = "path:./0_1_2";
+  inputs."funchook-0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."funchook-0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''funchook wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-funchook-master.flake = false;
+  inputs.src-funchook-master.owner = "ba0f3";
+  inputs.src-funchook-master.ref   = "refs/heads/master";
+  inputs.src-funchook-master.repo  = "funchook.nim";
+  inputs.src-funchook-master.type  = "github";
+  
+  inputs."https://github.com/ba0f3/distorm3.nim".dir   = "nimpkgs/h/https://github.com/ba0f3/distorm3.nim";
+  inputs."https://github.com/ba0f3/distorm3.nim".owner = "riinr";
+  inputs."https://github.com/ba0f3/distorm3.nim".ref   = "flake-pinning";
+  inputs."https://github.com/ba0f3/distorm3.nim".repo  = "flake-nimble";
+  inputs."https://github.com/ba0f3/distorm3.nim".type  = "github";
+  inputs."https://github.com/ba0f3/distorm3.nim".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."https://github.com/ba0f3/distorm3.nim".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-funchook-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-funchook-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
