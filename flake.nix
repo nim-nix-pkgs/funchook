041a27bc201063b00c1056139621e4e647612fd2
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:45 -0300

    chore: re index funchook

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."funchook-master".dir   = "master";
+  inputs."funchook-master".owner = "nim-nix-pkgs";
+  inputs."funchook-master".ref   = "master";
+  inputs."funchook-master".repo  = "funchook";
+  inputs."funchook-master".type  = "github";
+  inputs."funchook-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."funchook-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."funchook-0_1_1".dir   = "0_1_1";
+  inputs."funchook-0_1_1".owner = "nim-nix-pkgs";
+  inputs."funchook-0_1_1".ref   = "master";
+  inputs."funchook-0_1_1".repo  = "funchook";
+  inputs."funchook-0_1_1".type  = "github";
+  inputs."funchook-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."funchook-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."funchook-0_1_2".dir   = "0_1_2";
+  inputs."funchook-0_1_2".owner = "nim-nix-pkgs";
+  inputs."funchook-0_1_2".ref   = "master";
+  inputs."funchook-0_1_2".repo  = "funchook";
+  inputs."funchook-0_1_2".type  = "github";
+  inputs."funchook-0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."funchook-0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
